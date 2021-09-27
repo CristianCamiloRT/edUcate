170f4aa75f5b79d510831509fca0cdd9bdda1a93
Edward201 <44841164+Edward201@users.noreply.github.com>
2021-09-26 20:56:48 -0500

    Agregar footer

--- application/views/layout.php
+++ application/views/layout.php
@@ -17,6 +17,7 @@
     <link href="assets\img\icons\reproductor-de-video.png" type="image/png" sizes="128x128" rel="shortcut icon" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css?v=<?php echo date("YmdHi")?>" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.min.css?v=<?php echo date("YmdHi")?>">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body class="bg-body">
@@ -25,7 +26,8 @@
 		$this->load->model('areas_model');   
 		$CI = get_instance();
 		$areas = $CI->areas_model->get_areas();
-		$this->load->view('sections/navbar', ['areas' => $areas, 'CI' => $CI]); 
+		$this->load->view('sections/navbar', ['areas' => $areas, 'CI' => $CI]);
+		$this->load->view('sections/footer');
 	?>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js?v=<?php echo date("YmdHi")?>" crossorigin="anonymous"></script>
---
+++ application/views/sections/footer.php
@@ -0,0 +1,21 @@
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-sm-6">
+                <h3>Creadores</h3>
+                <ul>
+                    <li>Juan Andres Martinez Chavez</li>
+                    <li>Gina Carolina Paz Romero</li>
+                    <li>Cristian Camilo Ramirez Torres</li>
+                    <li>Edward Santiago Rodriguez Lopez</li>
+                    <li>Saira Katherinne Vega Martin</li>
+                </ul>
+            </div>
+            <div class="col-lg-6 col-md-6 ">
+                <h3>edUcate</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt possimus, autem quis ipsa hic delectus cupiditate ea, optio quos dicta quas doloremque debitis deserunt, amet earum tempore dignissimos nemo accusantium.</p>
+            </div>
+        </div>
+        <p class="copyright">edUcate © 2021</p>
+    </div>
+</div>
