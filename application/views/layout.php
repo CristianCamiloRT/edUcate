9d1198c42585cf4aa496a83c35dc3f898ddf1fad
CristianCamiloRT <cristianramirez.desarrollador@hotmail.com>
2021-09-26 20:52:50 -0500

    Ajustes en vista de videos

--- application/views/layout.php
+++ application/views/layout.php
@@ -28,6 +28,8 @@
 		$this->load->view('sections/navbar', ['areas' => $areas, 'CI' => $CI]); 
 	?>
 
+    {_content}
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js?v=<?php echo date("YmdHi")?>" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js?v=<?php echo date("YmdHi")?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js?v=<?php echo date("YmdHi")?>" crossorigin="anonymous"></script>
