1b6bb46f9e8bcb6cbf846e805e2c8720412aa3ce
CristianCamiloRT <cristianramirez.desarrollador@hotmail.com>
2021-09-26 22:46:31 -0500

    Footer a corregir

--- application/views/layout.php
+++ application/views/layout.php
@@ -31,8 +31,13 @@
 
     {_content}
 
-    <?php $this->load->view('sections/footer'); ?>
-    
+    <?php 
+    if ($this->router->fetch_method() == "ver") {
+        $this->load->view('sections/footer2');
+    } else {
+        $this->load->view('sections/footer'); 
+    }
+    ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js?v=<?php echo date("YmdHi")?>" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js?v=<?php echo date("YmdHi")?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js?v=<?php echo date("YmdHi")?>" crossorigin="anonymous"></script>
