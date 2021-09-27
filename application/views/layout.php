210556eb038557b2e949184bb2ff1f42ccb2ea4e
CristianCamiloRT <cristianramirez.desarrollador@hotmail.com>
2021-09-26 21:14:06 -0500

    Ajustes en footer

--- application/views/layout.php
+++ application/views/layout.php
@@ -27,11 +27,12 @@
 		$CI = get_instance();
 		$areas = $CI->areas_model->get_areas();
 		$this->load->view('sections/navbar', ['areas' => $areas, 'CI' => $CI]);
-		$this->load->view('sections/footer');
 	?>
 
     {_content}
 
+    <?php $this->load->view('sections/footer'); ?>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js?v=<?php echo date("YmdHi")?>" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js?v=<?php echo date("YmdHi")?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js?v=<?php echo date("YmdHi")?>" crossorigin="anonymous"></script>
